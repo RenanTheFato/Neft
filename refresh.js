67758091985c6fd708960d2a77b8f363bb0b3c87
RenanTheFato <138338709+RenanTheFato@users.noreply.github.com>
2023-07-13 17:30:06 -0300

    Add files via upload
    Kewboard recovery, come back to code. Added new page, change code in virtually all files, added jquery sistem to load page, fixed issues and add new features.

---
+++ refresh.js
@@ -0,0 +1,11 @@
+$( "#planos" ).click(function() {
+   $("main").load("planos.html");
+  } );
+     
+$( "#sobre" ).click(function() {
+   $("#main").load("refresh.js");
+  } );
+
+$( "#contato" ).click(function() {
+   $("#main").load("script.js");
+  } );
--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-var menu = document.querySelector('.menu')
-let nav = document.querySelector('.nav-menu')
+document.addEventListener('DOMContentLoaded', function () {
+    var menu = document.querySelector('.menu')
+    let nav = document.querySelector('.nav-menu')
 
-menu.addEventListener('click', clicar)
+    menu.addEventListener('click', function () {
+        menu.classList.toggle('Active')
+        nav.classList.toggle('Active')
+        console.log('Menu Ativo')  
+    })
+    
+    var items = document.getElementsByTagName("a")
+    for(var it = 0; it < items.length; it++){
+        items[it].addEventListener('click', function(){
+            menu.classList.remove('Active')
+            nav.classList.remove('Active')
+            console.log('Menu Desativado') 
+        })
+    }
+    var checkOpacity = true
+    var btn1 = document.querySelector('.start')
+    var btn2 = document.querySelector('.planos')
+    var img = document.querySelector('.computer')
 
-function clicar(){
-    menu.classList.toggle('Active')
-    nav.classList.toggle('Active')
-    console.log('Menu Ativo')
-}
+    menu.addEventListener('click', function(){
+    if (checkOpacity) {
+        btn1.style.visibility = 'hidden'
+        btn2.style.visibility = 'hidden'
+        img.style.visibility = 'hidden'
+        checkOpacity = false
+    } else {
+        btn1.style.visibility = 'visible'
+        btn2.style.visibility = 'visible'
+        img.style.visibility = 'visible'
+        checkOpacity = true
+    }
+
+    })
+})
 
 function imageChange() {
     var picture = document.querySelector('.simbol')
@@ -32,24 +60,4 @@ window.addEventListener('resize', function () {
     console.log('Redimensionou')
 })
 
-
-var checkOpacity = true
-var btn1 = document.querySelector('.start')
-var btn2 = document.querySelector('.planos')
-var img = document.querySelector('.computer')
-
-menu.addEventListener('click', function(){
-    if (checkOpacity) {
-        btn1.style.visibility = 'hidden'
-        btn2.style.visibility = 'hidden'
-        img.style.visibility = 'hidden'
-        checkOpacity = false
-    } else {
-        btn1.style.visibility = 'visible'
-        btn2.style.visibility = 'visible'
-        img.style.visibility = 'visible'
-        checkOpacity = true
-    }
-
-})
 
