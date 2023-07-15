b7d240066d7e412dc55a04a6f323c0c66cea230f
RenanTheFato <138338709+RenanTheFato@users.noreply.github.com>
2023-07-15 15:20:24 -0300

    Add files via upload
    Correction of some bugs. Updated the Final Version. Project is Complete.

--- refresh.js
+++ refresh.js
@@ -2,15 +2,11 @@ $( "#planos" ).click(function() {
    $("main").load("planos.html");
   } );
 
-  $('.planos').click(function(){
+  $('.planos').click(function() {
    $('main').load("planos.html")
 });
   
      
 $( "#sobre" ).click(function() {
    $("#main").load("sobre.html");
-  } );
-
-$( "#contato" ).click(function() {
-   $("#main").load("script.js");
   } );
